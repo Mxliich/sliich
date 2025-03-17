762e2bb87e4b3383e467a70cfcc7a88c953822d1
Mxliich <marawanmahmoud200988@gmail.com>
2025-03-17 19:46:00 +0200

    fix: type error and update config

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['sliich.vercel.app'], // تم تحديث النطاق
+  },
+}
+
+module.exports = nextConfig 
